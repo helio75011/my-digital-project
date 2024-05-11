54f71cb0f29ae5851cba91c60a5a6f0b1aa67b11
helio75011 <debreynehelio@gmail.com>
2024-05-11 19:02:26 +0200

    add news pages

--- frontend/src/Pages/Acceuil/Acceuil.jsx
+++ frontend/src/Pages/Acceuil/Acceuil.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Acceuil.css'
-import Header from '../../components/Header/Header'
+import Header from '../../components/Section/Header/Header'
 import Set from '../../components/Section/Set/Set'
 import Statistics from '../../components/Section/Statistics/Statistics'
 import WhoWeHelp from '../../components/Section/WhoWeHelp/WhoWeHelp'
@@ -12,7 +12,7 @@ import PricingPlan from '../../components/Section/PricingPlan/PricingPlan'
 import Testimonials from '../../components/Section/Testimonials/Testimonials'
 import FAQ from '../../components/Section/FAQ/FAQ'
 import Contact from '../../components/Section/Contact/Contact'
-import Footer from '../../components/Footer/Footer'
+import Footer from '../../components/Section/Footer/Footer'
 const Acceuil = () => {
   return (
     <div className='body-landing'>
